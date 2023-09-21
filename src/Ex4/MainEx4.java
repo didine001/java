9a16f3736f55fc552685fdae81ceb45b82db4350
didine712 <aguenassia@gmail.com>
2023-09-21 11:49:31 +0200

    changes

--- src/Ex4/MainEx4.java
+++ src/Ex4/MainEx4.java
@@ -2,20 +2,16 @@ package Ex4;
 
 public class MainEx4 {
     public static void main(String[] args) {
+
         Singleton singleton1 = Singleton.getInstance();
         Singleton singleton2 = Singleton.getInstance();
 
-        int adresseMemoire1 = System.identityHashCode(singleton1);
-        int adresseMemoire2 = System.identityHashCode(singleton2);
-
-        System.out.println("Adresse mémoire du premier singleton : " + adresseMemoire1);
-        System.out.println("Adresse mémoire du deuxième singleton : " + adresseMemoire2);
-
-        if (adresseMemoire1 == adresseMemoire2) {
+        System.out.println("Adresse mémoire du premier singleton : " + singleton1);
+        System.out.println("Adresse mémoire du deuxième singleton : " + singleton2);
+        if (singleton1 == singleton2) {
             System.out.println("Les deux instances ont la même adresse mémoire.");
         } else {
             System.out.println("Les deux instances ont des adresses mémoire différentes.");
         }
     }
 }
-
